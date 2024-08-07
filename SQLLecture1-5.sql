9fa3f96016a568957aee45e18a153ab2d10913a0
siddhant ashok <siddhant.ashok@gingerwebs.in>
2024-08-07 13:16:19 +0530

    Create | Alter | Drop | Use of System Stored Procedure

---
+++ SQLLecture1-5.sql
@@ -0,0 +1,23 @@
+
+--Create DB
+Create Database Sample2
+
+--Alter DB
+Alter Database Sample2 Modify Name = SampleNew
+
+-- Rename Database using System defined Stored Procedure
+Execute sp_renameDB 'SampleNew','NewSample'
+
+
+-----------
+use ICAIADV_12_JAN_2021
+Execute sp_helpIndex 'RESULT_EXAMINATION'			-- System SP to get info of all the indexes present inside the table
+
+select * from RESULT_EXAMINATION
+
+Execute sp_help 'RESULT_EXAMINATION'	-- System SP to get information about the particular table
+
+
+--Drop/Delete the database
+Alter Database Sample2 Set Single_User With Rollback Immediate --Normally DB's are in Multi_User mode. So to delete/drop a Db first put Db in Single_User mode
+Drop Database Sample2
