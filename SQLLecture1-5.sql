8e73701b314c3b27ea954536ace6b746f954b4f6
siddhant ashok <siddhant.ashok@gingerwebs.in>
2024-08-10 15:24:36 +0530

    midway commit of lecture 3

--- SQLLecture1-5.sql
+++ SQLLecture1-5.sql
@@ -1,6 +1,6 @@
 
 --Create DB
-Create Database Sample2
+Create Database SampleDB
 
 --Alter DB
 Alter Database Sample2 Modify Name = SampleNew
@@ -11,6 +11,7 @@ Execute sp_renameDB 'SampleNew','NewSample'
 
 -----------
 use ICAIADV_12_JAN_2021
+Go
 Execute sp_helpIndex 'RESULT_EXAMINATION'			-- System SP to get info of all the indexes present inside the table
 
 select * from RESULT_EXAMINATION
@@ -21,3 +22,23 @@ Execute sp_help 'RESULT_EXAMINATION'	-- System SP to get information about the p
 --Drop/Delete the database
 Alter Database Sample2 Set Single_User With Rollback Immediate --Normally DB's are in Multi_User mode. So to delete/drop a Db first put Db in Single_User mode and roll back all current active session/transactions for that DB
 Drop Database Sample2
+
+
+---Creating and Working with tables
+
+use SampleDB
+Go
+create table tblPerson(
+	ID int Primary Key not null,
+	[Name] nvarchar(50) not null,
+	Email nvarchar(50) not null,
+	GenderId int 
+)
+
+
+create table tblGender(
+	ID int Primary key not null,
+	Gender nvarchar(50) not null
+)
+
+Alter table tblPerson add constraint tblGender_GenderId_FK Foreign key GenderId references tblGender(ID)
