a378914f0e8f0ffb838f4a22d2f830e9be4c3326
siddhant ashok <siddhant.ashok@gingerwebs.in>
2024-08-16 15:18:42 +0530

    More string functions LEFT(), RIGHT(), CHARINDEX(), SUBSTRING()

--- SQLLecture21-25.sql
+++ SQLLecture21-25.sql
@@ -49,3 +49,40 @@ Select Reverse(LTrim(RTrim('	Hello	')))
 Select * from tblEmployee
 Select [Name], LEN([Name]) as [Total Characters] from tblEmployee
 
+
+---LECTURE 23 : FEW MORE STRING FUNCITONS - LEFT(), RIGHT(), CHARINDEX(), SUBSTRING() etc ---
+Select Left('ABCDEF', 3)
+Select Right('ABCDEF', 3)
+Select CHARINDEX('@','Sara@aaa.com',1)
+
+Select SUBSTRING('ABCDEFGH', 1, 4)
+
+
+Select SUBSTRING('Silvester@aaa.com', 1, CHARINDEX('@','Silvester@aaa.com',2) - 1)	--extract name from email id
+
+Select SUBSTRING('Silvester@aaa.com', CHARINDEX('@', 'Silvester@aaa.com', 2)+ 1, LEN('Silvester@aaa.com') - CHARINDEX('@', 'Silvester@aaa.com', 2))
+Select SUBSTRING('Silvest@aaa.com', CHARINDEX('@', 'Silvest@aaa.com', 2)+ 1, LEN('Silvest@aaa.com') - CHARINDEX('@', 'Silvest@aaa.com', 2))
+
+
+
+Select * from tblPerson
+Select * from tblGender
+
+sp_help tblPerson
+
+Insert into tblGender values(3, 'Unknown')
+
+
+Insert into tblPerson values(1,'Sara', 'sara@aaa.com', 2)
+Insert into tblPerson values(2,'Pam', 'pam@aaa.com', 2)
+Insert into tblPerson values(3,'Todd', 'todd@aaa.com', 1)
+Insert into tblPerson values(4,'Russel', 'russel@bbb.com', 1)
+Insert into tblPerson values(5,'Ben', 'ben@bbb.com', 1)
+Insert into tblPerson values(6,'Valarie', 'valarie@bbb.com', 2)
+Insert into tblPerson values(7,'Hudson', 'hudson@ccc.com', 3)
+Insert into tblPerson values(8,'Sam', 'sam@ccc.com', 1)
+
+Select * from tblPerson
+
+Select SUBSTRING(Email, CHARINDEX('@', Email)+ 1, LEN(Email) - CHARINDEX('@', Email)) as EmailDomain, Count(Email) as Total from tblPerson 
+Group by SUBSTRING(Email, CHARINDEX('@', Email)+ 1, LEN(Email) - CHARINDEX('@', Email))
