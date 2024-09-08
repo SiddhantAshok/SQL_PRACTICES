70878ada8cddc8e10b98191260f2a6ca0def2f33
Siddhant Ashok <siddhant.ashok091994@gmail.com>
2024-09-08 14:29:16 +0530

    Triggers | DML Triggers | After Insert and After Update Trigger

--- SQLLecture41-45.sql
+++ SQLLecture41-45.sql
@@ -101,3 +101,65 @@ as
 Select Id, Name, Gender from ##TestTempTable
 
 
+---LECTURE 43 : TRIGGERS ---
+
+--In SQL server there are 3 types of triggers
+--	1. DML triggers
+--	2. DDL triggers
+--	3. Logon triggers
+
+--	DML triggers are fired automatically in response to DML events(INSERT, UPDATE & DELETE)
+
+--DML triggers can be further classified into 2 types
+--	1. After triggers(Sometimes called as FOR triggers)
+--	2. Instead of triggers
+
+--After triggers, as the name says, fires after the triggering action. The INSERT, UPDATE, and DELETE statements, causes an after trigger to fire after the respective statements complete execution.
+
+--INSTEAD of triggers, fires instead of the triggering action. The INSERT, UPDATE, and DELETE statements, can cause an INSTEAD OF trigger to fire INSTEAD OF the respective statement execution.
+
+
+
+Create Table tblEmployeeAudit
+(
+	Id int identity(1,1) primary key,
+	AuditData nvarchar(1000)
+)
+
+Select * from tblEmployeeAudit
+Select * from tblEmployee
+
+Alter Trigger tr_tblEmployee_ForInsert
+On tblEmployee
+FOR INSERT
+As
+Begin
+	--Select * from inserted
+	Declare @Id int
+	Select @Id = Id from inserted
+	
+	insert into tblEmployeeAudit values (CONCAT('New employee with Id = ', Cast(@Id as nvarchar(5)), ' is added at ', Cast(GetDate() as nvarchar(20))))
+End
+
+--In the trigger, we are getting the id from inserted table. So, what is this inserted table? INSERTED table, is a special table used by DML triggers. 
+--When you add a new row into tblEmployee table, a copy of the row will also be made into inserted table, which only a trigger can access. You cannot access this table outside the context of the trigger.
+
+
+sp_help tblEmployee
+
+Create Trigger tr_tblEmployee_ForDelete
+On tblEmployee
+For Delete
+As
+Begin
+	Declare @Id int
+	Select @Id = Id from deleted
+
+	insert into tblEmployeeAudit values (Concat('An existing employee with Id = ', Cast(@Id as nvarchar(5)), ' is deleted at ', Cast(GetDate() as nvarchar(20))))
+End
+
+Insert into tblEmployee values ('Jack', 'Male', 5000, 'New Orleans', 1)
+Insert into tblEmployee values ('Mosh', 'Male', 4100, 'Hisrat', 2)
+Insert into tblEmployee values ('Dave', 'Male', 5700, 'New York', 1)
+
+Delete from tblEmployee where Id = 109
